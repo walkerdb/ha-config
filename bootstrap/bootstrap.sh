fc8fa23aff4681b114cc5d81536eef180681d485
walkerdb <wboyle13@gmail.com>
2017-08-03 01:44:59 -0400

    add bootstrap script & service file; add circleci pipeline

---
+++ bootstrap/bootstrap.sh
@@ -0,0 +1,9 @@
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install libudev-dev -y
+sudo apt-get install libpcap-dev -y
+sudo apt-get install npm -y
+sudo pip3 install sqlalchemy
+sudo pip3 install phue
+sudo pip3 install python_openzwave
+pip3 install homeassistant
