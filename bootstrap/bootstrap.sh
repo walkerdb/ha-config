c56b462334c84b69217f1130571894ab6fd8b29e
walkerdb <wboyle13@gmail.com>
2017-08-05 11:31:29 -0500

    add dasher files; update circleci pipeline

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -1,9 +1,18 @@
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt-get install libudev-dev -y
-sudo apt-get install libpcap-dev -y
-sudo apt-get install npm -y
-sudo pip3 install sqlalchemy
-sudo pip3 install phue
-sudo pip3 install python_openzwave
-pip3 install homeassistant
+sudo apt-get install libudev-dev libpcap-dev npm node -y
+wget http://node-arm.herokuapp.com/node_latest_armhf.deb
+sudo dpkg -i node_latest_armhf.deb
+
+# install dasher
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+nvm install 8
+git clone https://github.com/maddox/dasher.git
+cd dasher
+npm install
+cd ..
+
+sudo pip3 install sqlalchemy phue python_openzwave homeassistant
