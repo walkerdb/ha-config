cded785c9439b56f4f76611a6de50621fd1b9912
walkerdb <wboyle13@gmail.com>
2017-11-23 01:08:34 +0000

    add update_hass script

---
+++ update_hass.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo 'stopping home assistant...'
+sudo systemctl stop home-assistant.service >&2
+
+echo 'installing updates...'
+sudo python3 -m pip install --upgrade homeassistant >&2
+hass --script check_config >&2
+
+echo 'starting home assistant...'
+sudo systemctl start home-assistant.service >&2
+
+echo 'done! hass is starting now.'
